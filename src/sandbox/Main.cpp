ee23f80c91cbf8b367984f827fa1005700a6286b
schlangster <schlangster@gmail.com>
2014-02-22 19:26:10 +0100

    Another example.

--- src/sandbox/Main.cpp
+++ src/sandbox/Main.cpp
@@ -8,8 +8,10 @@ using namespace std;
 
 REACTIVE_DOMAIN(TestDomain, SourceSetEngine);
 
-void SignalExample()
+void SignalExample1()
 {
+	cout << "Signal Example 1" << endl;
+
 	auto width = TestDomain::MakeVar(60);
 	auto height = TestDomain::MakeVar(70);
 	auto depth = TestDomain::MakeVar(8);
@@ -27,11 +29,43 @@ void SignalExample()
 	cout << "t1" << endl;
 	cout << "\tArea: " << area() << endl;
 	cout << "\tVolume: " << volume() << endl;
+
+	auto customFunc = (width,height,depth) >>= [] (int w, int h, int d) {
+		return w * h * d * 1000;
+	};
+
+	cout << endl;
+}
+
+void SignalExample2()
+{
+	cout << "Signal Example 2" << endl;
+
+	auto width = TestDomain::MakeVar(60);
+	auto height = TestDomain::MakeVar(70);
+	auto depth = TestDomain::MakeVar(8);
+
+	auto volume = (width,height,depth) >>= [] (int w, int h, int d) {
+		return w * h * d;
+	};
+
+	Observe(volume, [] (int v) {
+		cout << "Volume changed to: " << v << endl;
+	});
+
+	{
+		TestDomain::ScopedTransaction _;
+		width <<= 90;
+		depth <<= 80;
+	}
+
+	cout << endl;
 }
 
 int main()
 {
-	SignalExample();
+	SignalExample1();
+	SignalExample2();
 
 	return 0;
 }
