f1ecb2a4399017f37ed506d5a0ca23001828a1da
schlangster <schlangster@gmail.com>
2014-02-23 01:59:31 +0100

    Added ObjectExample.

--- src/sandbox/Main.cpp
+++ src/sandbox/Main.cpp
@@ -3,10 +3,16 @@
 #include "react/Signal.h"
 #include "react/propagation/SourceSetEngine.h"
 
+#include "react/ReactiveObject.h"
+
 using namespace react;
 using namespace std;
 
+// Defines a domain.
+// Each domain represents a separate dependency graph, managed by a dedicated propagation engine.
+// Reactives of different domains can not be combined.
 REACTIVE_DOMAIN(TestDomain, SourceSetEngine);
+
 
 void SignalExample1()
 {
@@ -30,10 +36,6 @@ void SignalExample1()
 	cout << "\tArea: " << area() << endl;
 	cout << "\tVolume: " << volume() << endl;
 
-	auto customFunc = (width,height,depth) >>= [] (int w, int h, int d) {
-		return w * h * d * 1000;
-	};
-
 	cout << endl;
 }
 
@@ -49,6 +51,10 @@ void SignalExample2()
 		return w * h * d;
 	};
 
+	// Observe returns an observer handle, which can be used to detach the observer explicitly.
+	// This observer handle holds a shared_ptr to the subject, so as long as it exists,
+	// the subject will not be destroyed.
+	// The lifetime of the observer itself is tied to the subject.
 	Observe(volume, [] (int v) {
 		cout << "Volume changed to: " << v << endl;
 	});
@@ -79,9 +85,51 @@ void EventExample1()
 	numbers2 << 40 << 50 << 60;
 
 	cout << endl;
+}
 
-	// Note: Observer must be detached explicitly.
-	// This is likely to change in the future.
+
+class Person : public ReactiveObject<TestDomain>
+{
+public:
+	VarSignal<int>	Age		= MakeVar(1);
+	Signal<int>		Health	= 100 - Age;
+	Signal<int>		Wisdom  = Age * Age / 100;
+
+	// Note: Initializing them directly uses the same lambda for both signals...
+	// Compiler bug?
+	Observer	wisdomObs;
+	Observer	weaknessObs;
+
+	Person()
+	{
+		wisdomObs = Observe(Wisdom > 50, [] (bool isWise)
+		{
+			if (isWise)	cout << "I'll do it next week!" << endl;
+			else		cout << "I'll do it next month!" << endl;
+		});
+
+		weaknessObs = Observe(Health < 25, [] (bool isWeak)
+		{
+			if (isWeak)	cout << ":<" << endl;
+			else		cout << ":D" << endl;
+		});
+	}
+};
+
+void ObjectExample1()
+{
+	cout << "Object Example 1" << endl;
+
+	Person somePerson;
+
+	somePerson.Age <<= 30;
+	somePerson.Age <<= 60;
+	somePerson.Age <<= 90;
+
+	cout << "Health: " << somePerson.Health() << endl;
+	cout << "Wisdom: " << somePerson.Wisdom() << endl;
+
+	cout << endl;
 }
 
 int main()
@@ -89,6 +137,8 @@ int main()
 	SignalExample1();
 	SignalExample2();
 	EventExample1();
+
+	ObjectExample1();
 
 	return 0;
 }
