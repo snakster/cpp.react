357406bb9d76f9c5b76693d693770d6aa489362e
schlangster <schlangster@gmail.com>
2014-03-06 16:44:38 +0100

    Added BlockingCondition.

---
+++ include/react/common/Concurrency.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+
+namespace react {
+
+class BlockingCondition
+{
+public:
+	inline void Block()
+	{// mutex_
+		std::lock_guard<std::mutex> scopedLock(mutex_);
+		blocked_ = true;
+	}// ~mutex_
+
+	inline void Unblock()
+	{// mutex_
+		std::lock_guard<std::mutex> scopedLock(mutex_);
+		blocked_ = false;
+		condition_.notify_all();
+	}// ~mutex_
+
+	inline void WaitForUnblock()
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+		while (blocked_)
+			condition_.wait(lock);
+	}
+
+	inline bool IsBlocked()
+	{// mutex_
+		std::lock_guard<std::mutex> scopedLock(mutex_);
+		return blocked_;
+	}// ~mutex_
+
+	template <typename F>
+	inline void Run(F func)
+	{// mutex_
+		std::lock_guard<std::mutex> scopedLock(mutex_);
+		func(blocked_);
+	}// ~mutex_
+
+	template <typename F>
+	inline bool RunIfBlocked(F func)
+	{// mutex_
+		std::lock_guard<std::mutex> scopedLock(mutex_);
+
+		if (!blocked_)
+			return false;
+
+		func();
+		return true;			
+	}// ~mutex_
+
+	template <typename F>
+	inline bool RunIfUnblocked(F func)
+	{// mutex_
+		std::lock_guard<std::mutex> scopedLock(mutex_);
+
+		if (blocked_)
+			return false;
+
+		func();
+		return true;			
+	}// ~mutex_
+
+private:
+	std::mutex				mutex_;
+	std::condition_variable	condition_;
+	bool					blocked_;
+};
+
+} // ~namespace react
