666e461a76ae1a1a881f7d26e8861f6ae581637e
schlangster <schlangster@gmail.com>
2014-03-12 02:53:06 +0100

    Released under the Boost Software License, Version 1.0.

--- include/react/common/GraphData.h
+++ include/react/common/GraphData.h
@@ -1,3 +1,9 @@
+
+//          Copyright Sebastian Jeckel 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <cstdio>
